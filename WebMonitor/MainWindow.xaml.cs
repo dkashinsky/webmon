25156bfb8e51e7be231c9db044eee29931947125
Dmitry Kashinsky <dmitry.kashinsky@gmail.com>
2018-11-30 00:07:07 +0400

    Tray Icon added

--- WebMonitor/MainWindow.xaml.cs
+++ WebMonitor/MainWindow.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -20,9 +21,26 @@ namespace WebMonitor
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		protected NotifyIcon TrayIcon { get; set; }
+
 		public MainWindow()
 		{
 			InitializeComponent();
+			
+			TrayIcon = new NotifyIcon
+			{
+				Icon = System.Drawing.Icon.FromHandle(Properties.Resources.Clover.GetHicon()),
+				Visible = true
+			};
+		}
+
+		private void Window_Closed(object sender, EventArgs e)
+		{
+			if (TrayIcon != null)
+			{
+				TrayIcon.Dispose();
+				TrayIcon = null;
+			}
 		}
 	}
 }
